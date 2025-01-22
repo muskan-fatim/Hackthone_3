0b031170ec143632be22c5217a612ea07d5ef677
Muskan Fatima <160476952+muskan-fatim@users.noreply.github.com>
2025-01-22 21:41:52 +0500

    Update navbar.tsx

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -41,28 +41,18 @@ const Navbar = () => {
         }`}
       >
         <div className="flex flex-col items-center py-6">
-        <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="40"
-  height="40"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="currentColor"
-  strokeWidth="2"
-  strokeLinecap="round"
-  strokeLinejoin="round"
-  className="mb-4"
->
-  <rect x="3" y="3" width="6" height="18" rx="1" ry="1"></rect>
-  <line x1="14" y1="5" x2="21" y2="5"></line>
-  <line x1="14" y1="12" x2="21" y2="12"></line>
-  <line x1="14" y1="19" x2="21" y2="19"></line>
-</svg>
+        
+
 
           <button
             className="text-lg font-semibold py-2 px-4 text-blue-500"
             onClick={() => router.push('/notification')}
-          >
+          ><Image
+                src="/notification-icon.png"
+                alt="Notification"
+                width={24}
+                height={24}
+              />
             Notifications
           </button>
           <button
@@ -79,13 +69,23 @@ const Navbar = () => {
       <nav className="flex flex-wrap items-center justify-between px-4 sm:px-8 py-4 bg-white shadow">
         <div className="md:hidden flex space-x-56">
           <div className="w-10 h-10 overflow-hidden cursor-pointer" onClick={toggleSidebar}>
-            <Image
-              src="/sidebar.png"
-              alt="Sidebar Icon"
-              width={40}
-              height={40}
-              className="object-cover"
-            />
+          <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="40"
+  height="40"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  strokeWidth="2"
+  strokeLinecap="round"
+  strokeLinejoin="round"
+  className="mb-4"
+>
+  <rect x="3" y="3" width="6" height="18" rx="1" ry="1"></rect>
+  <line x1="14" y1="5" x2="21" y2="5"></line>
+  <line x1="14" y1="12" x2="21" y2="12"></line>
+  <line x1="14" y1="19" x2="21" y2="19"></line>
+</svg>
           </div>
           <div className="w-10 h-10 overflow-hidden border border-gray-300 rounded-full">
           <Suspense fallback={<div className="text-sm text-gray-500">Loading...</div>}>
